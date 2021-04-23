6d1dc907337ff75d616743a8f66b9ef02fdab085
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-04-22 22:02:22 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ keys.forEach(function (key) {
 
   key.addEventListener('click', function (e) {
     //console.log(e.path[1].dataset.key);
-    e.keyCode = e.path[1].dataset.key;
+    e.keyCode = e.composedPath[1].dataset.key;
     playSound(e);
   });
 });
